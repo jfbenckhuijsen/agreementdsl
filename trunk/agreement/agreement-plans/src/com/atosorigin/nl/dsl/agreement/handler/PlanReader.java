15f6d61dc973ded25f6ddbb7d969213c0203f1c0
jeroen.benckhuijsen <jeroen.benckhuijsen@9b012de6-b151-0410-a661-a9f7e6f6e79b>
2008-11-17 17:34:57 +0000

    Run fixes and cleanup

--- trunk/agreement/agreement-plans/src/com/atosorigin/nl/dsl/agreement/handler/PlanReader.java
+++ trunk/agreement/agreement-plans/src/com/atosorigin/nl/dsl/agreement/handler/PlanReader.java
@@ -5,10 +5,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
+import org.eclipse.emf.ecore.EPackage;
 import org.openarchitectureware.xtext.parser.parsetree.Node;
 
+import com.atosorigin.nl.dsl.agreement.AgreementPackage;
 import com.atosorigin.nl.dsl.agreement.Plan;
 import com.atosorigin.nl.dsl.agreement.parser.XtextParser;
+import com.atosorigin.nl.dsl.agreement.resource.AgreementResourceFactory;
 
 /**
  * Class to read an instance of the Agreement DSL.
@@ -17,9 +20,22 @@ import com.atosorigin.nl.dsl.agreement.parser.XtextParser;
  */
 public class PlanReader {
 
+	private static boolean switched = false;
 	private Plan plan;
 
 	public PlanReader(InputStream in) {
+		synchronized (PlanReader.class) {
+			if (!switched) {
+				// Switch to static EMF
+				AgreementResourceFactory.register();
+				EPackage agreementPackage = AgreementPackage.eINSTANCE;
+				EPackage.Registry.INSTANCE.put(agreementPackage.getNsURI(),
+						agreementPackage);
+				switched = true;
+			}
+		}
+		
+		// Parse the input stream
 		XtextParser parser = new XtextParser(in);
 		Node node = parser.getRootNode();
 		this.plan = (Plan) node.getModelElement();
